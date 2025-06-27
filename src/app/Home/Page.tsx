c2cfd4772ebac4a1804c5d700dbc711460dae338
shivshankarcode <shivshankarmoney@gmail.com>
2025-06-27 19:46:19 +0530

    minor changes in home page

--- src/app/Home/Page.tsx
+++ src/app/Home/Page.tsx
@@ -1,13 +1,12 @@
-import Navbar from "./Navbar";
+"use client";
 import intro_photo from "../../images/home_image.webp";
 import Image from "next/image";
 export default function Page() {
   return (
     <div className="bg-gradient-to-br from-[#0d1738] to-[#504ba7]  text-white h-fit p-5 rounded-tr-2xl ">
-      {/* <Navbar /> */}
       <div className="flex flex-wrap md:flex-row items-center justify-center h-fit md:p-10 p-10 pt-20 gap-10">
         <div className="text-start  ">
-          <h2 className="text-5xl font-semibold">HI, I'm</h2>
+          <h2 className="text-5xl font-semibold">HI, I`{`&apos`}m`</h2>
           <h1 className="text-6xl font-bold">Shivshankar Kumar</h1>
           <h3 className="mt-4 text-3xl">Software Engineer</h3>
           <p className="mt-4 text-lg">I build full-stack web application</p>
